ffd9cd14fbb4241e9117a21af3eedcb719c07f89
Dmitry Chechotka <dmitry.chechotka@gmail.com>
2025-05-08 19:59:02 +0300

    feat: add data fetching for inventory JSON and populate table

--- data_fetch.js
+++ data_fetch.js
@@ -25,3 +25,31 @@ fetch('/data/vielas.json')
   .catch((error) => {
     console.error('Error loading JSON:', error);
   });
+
+fetch('/data/inventars.json')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((jsonData) => {
+    const tableBody = document.querySelector('#data-table tbody');
+
+    jsonData.forEach((row) => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+      <td>${row.id}</td>
+      <td>${row.nosaukums}</td>
+      <td>${row.tips}</td>
+      <td>${row.apakstips}</td>
+      <td>${row.skaits}</td>
+      <td></td>
+      <td>${row.komentari}</td>
+    `;
+      tableBody.appendChild(tr);
+    });
+  })
+  .catch((error) => {
+    console.error('Error loading JSON:', error);
+  });
